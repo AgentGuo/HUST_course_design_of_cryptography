0f604a8939cd20166476091199985d67eb5ad63e
panfeng <841796600@qq.com>
2020-09-23 11:20:20 +0800

    finally finish Montgomery

--- README.md
+++ README.md
@@ -12,13 +12,9 @@
 
 为了方便的使用S盒P盒的那些运算（直接使用数组， 而不使用位运算），所以想起了C语言课上学的**字段结构体和联合**，but程序跑太慢了~（最后一个数据大概有 $4 \times 10^6$ 个）
 
-![image](https://github.com/AgentGuo/HUST_course_design_of_cryptography/blob/master/image/2.png)
-
 #### 1.2 输入加速
 
 算法考试里常用的使用`getchar()`进行输入加速（也可以试试`fread()`哟），终于是快起来了（天翼3G太快啦~）。
-
-![image](https://github.com/AgentGuo/HUST_course_design_of_cryptography/blob/master/image/3.png)
 
 #### 1.3 再次加速
 
@@ -27,8 +23,6 @@
 速度跑的还是太慢了，影响后面线性分析的效率，所以又回来重写一波~
 
 听到大佬说打表跑得飞快，那我就不客气咯，遂即打了一个65536大小的表，果然起飞~~~（但与此同时，代码也飙升至4000+行，都是小问题~）
-
-![image](https://github.com/AgentGuo/HUST_course_design_of_cryptography/blob/master/image/4.png)
 
 ## 2.线性分析
 
@@ -124,6 +118,8 @@ p-1和q-1过于光滑就是gcd(p-1, q-1)不是太大？
 
 ## 7.综合5,6，完整实现RSA加密与解密过程
 
+[中国剩余定理](https://github.com/AgentGuo/HUST_course_design_of_cryptography/tree/master/RSAEncryptionAndDecryption)
+
 但是这里的解密过程不是真正的解密过程，这里是使用p，q并运用中国剩余定理解密的（实际情况不会这么解密~）
 
 啊啊啊，还以为综合5,6能够轻松秒杀T_T，跑得不够快，唉~
@@ -135,4 +131,18 @@ p-1和q-1过于光滑就是gcd(p-1, q-1)不是太大？
 3. **模除2加速**，模幂运算的过程中需要不对的对指数模除2，直接用除法效率并不高，可以使用`gmp_get_str`先将其转化为二进制字符串，然后再进行平方乘算法（感谢张同学点播^_^）
 
 啊啊啊，太不容易了，枯了，不过总算是能跑快快了，芜湖~
+
+## 8. 蒙哥马利算法
+
+[蒙哥马利算法](https://github.com/AgentGuo/HUST_course_design_of_cryptography/tree/master/RSAMontgomery)
+
+要人命！！gmp库居然没有移位运算，直接白给，跑的巨慢无比T_T
+
+都想放弃了，要不世界上库函数吧T_T！
+
+问问带佬咋做的，带佬还是猛，直接没用移位运算，用一个数组存好，直接移动指针，NB！
+
+枯了，昨晚调试到2点，心态崩了，太狗血了，本地`unsigned long`是32位，OJ是64位的，这也太坑爹了。。。
+
+还是睡醒了清醒，大早上半小时BUG就调好了，舒服了舒服了，终于过了，芜湖~~^_^
 
--- RSAMontgomery/源.cpp
+++ RSAMontgomery/源.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
-//#include<stack>
-//#include<stdio.h>
+#include<algorithm>
+#include<string>
+#include<string.h>
 #include "gmp.h"
 #pragma comment(lib,"libgmp-10.lib")
 //#define DEBUG 1
 using namespace std;
 mpz_t st[800];
-unsigned long long t[32];
-mpz_t R, k;
+char str1[1100], str2[1100];
+mpz_t R1, R2, k;
+mp_limb_t T1[64];
+mp_limb_t t0_in[4];
+mp_limb_t carry;
 //stack<int> que;
 void calInverse(mpz_t eInverse, mpz_t paraE, mpz_t paraN) {
+	// 使用辗转相除法求逆
 	int count = -1;
 	mpz_t n, temp, temp1, temp2, e;
 	mpz_init_set(e, paraE);
@@ -53,177 +58,124 @@ void calInverse(mpz_t eInverse, mpz_t paraE, mpz_t paraN) {
 	mpz_set(eInverse, temp1);
 	mpz_mod(eInverse, eInverse, paraN);
 }
-void expmod(mpz_t a, mpz_t paraE, mpz_t n) {
-	mpz_t e, eBack, aPow, temp;
-	mpz_init_set(e, paraE);
-	mpz_init_set(eBack, e);
-	mpz_init_set(aPow, a);
-	mpz_init(temp);
-
-
-	//mpz_set_ui(a, 1);
-	//
-	//while (mpz_cmp_ui(eBack, 0) != 0) {
-	//	mpz_mod_ui(temp, eBack, 2);
-	//	mpz_div_ui(eBack, eBack, 2);
-	//	if (mpz_cmp_ui(temp, 1) == 0) {
-	//		que.push(1);
-	//	}
-	//	else {
-	//		que.push(0);
-	//	}
-	//	//mpz_mod_ui(temp, eBack, 2);
-	//	//mpz_div_ui(eBack, eBack, 2);
-	//}
-	//while (!que.empty()) {
-	//	mpz_mul(a, a, a);
-	//	if (que.top()) {
-	//		mpz_mul(a, a, aPow);
-	//	}
-	//	mpz_mod(a, a, n);
-	//	que.pop();
-	//}
-
-	mpz_set_ui(a, 1);
-	mpz_mod_ui(temp, eBack, 2);	// 一些准备工作
-	while (mpz_cmp_ui(eBack, 0) != 0) {		// 当还没被除完时
-		if (mpz_cmp_ui(temp, 1) == 0) {		// 余数为1，则加上
-			mpz_mul(a, a, aPow);
-			mpz_mod(a, a, n);
-		}
-#ifdef DEBUG
-		gmp_printf("a = %Zd\n", a);
-		gmp_printf("temp = %Zd\n", temp);
-		gmp_printf("aPow = %Zd\n", aPow);
-		gmp_printf("eBack = %Zd\n", eBack);
-#endif // DEBUG
-
-		mpz_mul(aPow, aPow, aPow);		// 平方
-		mpz_mod(aPow, aPow, n);
-		mpz_div_ui(eBack, eBack, 2);
-		mpz_mod_ui(temp, eBack, 2);		// 求余数
+void MontMult(int n,  mpz_t T, mp_limb_t IN, mpz_t N) {
+	// 蒙哥马利乘 第层加速版本
+	mp_limb_t* pos = T1;
+	int T_size = T->_mp_size;
+	memcpy(T1, T->_mp_d, T_size * 8);
+	memset(T1 + T_size, 0, ((n << 1) + 1 - T_size) * 8);
+	int high_size = n + 1;
+	for (int i = 0; i < n; i++) {
+		mpn_mul_1(t0_in, pos, 1, IN);
+		carry = mpn_addmul_1(pos, N->_mp_d, n, t0_in[0]);
+		mpn_add_1(pos + n, pos + n, high_size, carry);
+		++pos;
+		--high_size;
 	}
-}
-void ChineseRemainderTheorem(mpz_t paraB1, mpz_t paraB2, mpz_t m1, mpz_t m2, mpz_t m1Inverse, mpz_t m2Inverse) {
-	mpz_t b1, b2, m;
-	mpz_init_set(b1, paraB1);
-	mpz_init_set(b2, paraB2);
-	mpz_init(m);
-	mpz_mul(m, m1, m2);
-	mpz_mul(b1, b1, m2);
-	mpz_mul(b1, b1, m2Inverse);
-	mpz_mul(b2, b2, m1);
-	mpz_mul(b2, b2, m1Inverse);
-	mpz_add(b1, b1, b2);
-	mpz_mod(b1, b1, m);
-	gmp_printf("%Zd\n", b1);
-}
-void MontMult(mpz_t T2, mpz_t A, mpz_t B, mpz_t IN, mpz_t N) {
-	mpz_t T1, t0, temp;
-	mpz_init(T1);
-	mpz_init(t0);
-	mpz_init(temp);
-	mpz_mul(T1, A, B);
-	for (int i = 0; i < 32; i++) {
-		mpz_mod(t0, T1, k);
-		mpz_mul(t0, t0, IN);
-		mpz_addmul(T1, N, t0);
-		mpz_mod(temp, T1, k);
-		mpz_div(T1, T1, k);
-	}
-	mpz_mod(T1, T1, N);
-	mpz_set(T2, T1);
+	if (mpn_cmp(pos, N->_mp_d, n) >= 0 || pos[n])
+		mpn_sub(pos, pos, n + 1, N->_mp_d, n);
+	//pos[n] &= 1;
+	memcpy(T->_mp_d, pos, n * 8);
+	T->_mp_size = n;
 }
 int main() {
-	int n, count;
-	mpz_t e, m, p, q, b1, b2, B, B1, B2, d, two, dBack, phi, temp, pInverse, qInverse, pIN, qIN, RpInverse, RqInverse;
+	//freopen("1.in", "r", stdin);
+	int n, length1, length2;
+	mpz_t e, m, p, q, _p, _q, d1, d2, b1, b2, B, B1, B2, d, two, dBack, phi, temp, pInverse, qInverse, pIN, qIN, RpInverse, RqInverse;
 	for (int i = 0; i < 800; i++) {
 		mpz_init(st[i]);
 	}
-	mpz_init(e);
-	mpz_init(m);
-	mpz_init(p);
-	mpz_init(q);
-	mpz_init(b1);
-	mpz_init(b2);
-	mpz_init(B);
-	mpz_init(B1);
-	mpz_init(B2);
-	mpz_init(d);
-	mpz_init(temp);
-	mpz_init(phi);
-	mpz_init(pInverse);
-	mpz_init(qInverse);
-	mpz_init(pIN);
-	mpz_init(qIN);
-	mpz_init(RpInverse);
-	mpz_init(RqInverse);
-	mpz_init(dBack);
-	mpz_init_set_ui(two, 2);
-	mpz_init(R);
-	
-	mpz_set_str(k, "4294967296", 10);
-	mpz_set_str(R, "179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216", 10);
+	mpz_init(e);mpz_init(m);mpz_init(p);mpz_init(q);mpz_init(_p);mpz_init(_q);mpz_init(b1);
+	mpz_init(b2);mpz_init(B);mpz_init(B1);mpz_init(B2);mpz_init(d);mpz_init(d1);mpz_init(d2);
+	mpz_init(temp);mpz_init(phi);mpz_init(pInverse);mpz_init(qInverse);mpz_init(pIN);mpz_init(qIN);
+	mpz_init(RpInverse);mpz_init(RqInverse);mpz_init(dBack);mpz_init_set_ui(two, 2);mpz_init(R1);
+	mpz_init(R2);
+
+	mpz_set_str(k, "18446744073709551616", 10);		// 2^64
 	gmp_scanf("%d%Zd%Zd%Zd", &n, p, q, e);
+
+	mpz_set(R1, k);
+	length1 = 1;
+	while (mpz_cmp(R1, p) <= 0) {		//	 对p,q分别求出R
+		mpz_mul(R1, R1, k);
+		length1++;
+	}
+	mpz_set(R2, k);
+	length2 = 1;
+	while (mpz_cmp(R2, q) <= 0) {
+		mpz_mul(R2, R2, k);
+		length2++;
+	}
+
+
 	mpz_mul(B, p, q);
-	calInverse(pInverse, p, q);
+	calInverse(pInverse, p, q);		// 中国剩余定理需要用到的逆
 	calInverse(qInverse, q, p);
-	calInverse(RpInverse, R, p);
-	calInverse(RqInverse, R, q);
-	calInverse(pIN, p, k);
+	calInverse(RpInverse, R1, p);	// 蒙哥马利R的逆
+	calInverse(RqInverse, R2, q);
+	calInverse(pIN, p, k);			// 负逆IN
 	mpz_sub(pIN, k, pIN);
 	calInverse(qIN, q, k);
 	mpz_sub(qIN, k, qIN);
-	mpz_sub_ui(temp, p, 1);
-	mpz_sub_ui(phi, q, 1);
-	mpz_mul(phi, phi, temp);
-	calInverse(d, e, phi);
-	for (int i = 0; i < n; i++) {
-		gmp_scanf("%Zd", m);
+	mpz_sub_ui(_p, p, 1);
+	mpz_sub_ui(_q, q, 1);
+	mpz_mul(phi, _p, _q);
+	calInverse(d, e, phi);			// 求出解密指数d
+	mpz_mod(d1, d, _p);				// 解密指数分别模除p-1和q-1，中国剩余定理加速
+	mpz_mod(d2, d, _q);
+	mpz_get_str(str1, 2, d1);		// 将解密指数转化位字符串，加速计算
+	mpz_get_str(str2, 2, d2);
 
-		mpz_set(B1, m);
-		mpz_mul(B1, B1, R);
+	for (int i = 0; i < n; i++) {
+		gmp_scanf("%Zd", m);		// 输入m
+		mpz_set(B1, m);				// 转化位蒙哥马利数
+		mpz_mul(B1, B1, R1);
 		mpz_mod(B1, B1, p);
-		mpz_set(b1, R);
+		mpz_set(b1, R1);
 		mpz_mod(b1, b1, p);
 
 		mpz_set(B2, m);
-		mpz_mul(B2, B2, R);
+		mpz_mul(B2, B2, R2);
 		mpz_mod(B2, B2, q);
-		mpz_set(b2, R);
+		mpz_set(b2, R2);
 		mpz_mod(b2, b2, q);
 
-		mpz_set(dBack, d);
-		while (mpz_cmp_ui(dBack, 0) != 0) {
-			mpz_fdiv_qr(dBack, temp, dBack, two);
-			if (mpz_cmp_ui(temp, 1) == 0) {
-				MontMult(b1, b1, B1, pIN, p);
-				MontMult(b2, b2, B2, qIN, q);
+
+		for (int i = 0; str1[i] != '\0'; i++) {		// 循环计算蒙哥马利乘
+			mpz_mul(b1, b1, b1);
+			MontMult(length1, b1, pIN->_mp_d[0], p);
+			if (str1[i] == '1') {
+				mpz_mul(b1, b1, B1);
+				MontMult(length1, b1, pIN->_mp_d[0], p);
 			}
-			MontMult(B1, B1, B1, pIN, p);
-			MontMult(B2, B2, B2, qIN, q);
+		}
+		for (int i = 0; str2[i] != '\0'; i++) {
+			mpz_mul(b2, b2, b2);
+			MontMult(length2, b2, qIN->_mp_d[0], q);
+			if (str2[i] == '1') {
+				mpz_mul(b2, b2, B2);
+				MontMult(length2, b2, qIN->_mp_d[0], q);
+			}
 		}
 
-		mpz_mul(b1, b1, RpInverse);
+		mpz_mul(b1, b1, RpInverse);		// 计算蒙哥马利逆
 		mpz_mod(b1, b1, p);
 		mpz_mul(b2, b2, RqInverse);
 		mpz_mod(b2, b2, q);
 
-		mpz_mul(b1, b1, q);
+		mpz_mul(b1, b1, q);				// 使用中国剩余定理计算最终结果
 		mpz_mod(b1, b1, B);
 		mpz_mul(b1, b1, qInverse);
 		mpz_mod(b1, b1, B);
 
 		mpz_mul(b2, b2, p);
 		mpz_mod(b2, b2, B);
-		//mpz_addmul(b1, b2, pInverse);
 		mpz_mul(b2, b2, pInverse);
 		mpz_mod(b2, b2, B);
 
 		mpz_add(b1, b1, b2);
 		mpz_mod(b1, b1, B);
 		gmp_printf("%Zd\n", b1);
-		//ChineseRemainderTheorem(b1, b2, p, q, pInverse, qInverse);
 	}
 	return 0;
 }
